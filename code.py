6788672a956e62cc778d8f8b64ac45f3acbcc564
Wesley <wesley.butura3@gmail.com>
2026-05-29 22:29:05 -0300

    feat: alteração da vulnerabilidade para teste(XSS: Cross-Site Scripting)

--- code.py
+++ code.py
@@ -1,20 +1,6 @@
-from flask import Flask, request
-import sqlite3
+from flask import request
 
-app = Flask(__name__)
-
-@app.route('/user')
-def buscar_usuario():
-
-    user_id = request.args.get("id")
-
-    conn = sqlite3.connect("banco.db")
-
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT * FROM users WHERE id = ?",
-        (user_id,)
-    )
-
-    return str(cursor.fetchone())
+@app.route('/hello')
+def hello():
+    name = request.args.get("name")
+    return f"<h1>Hello {name}</h1>"
