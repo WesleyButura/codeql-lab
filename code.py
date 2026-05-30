74bba7f8b6e092b104267d78ca036b9a38950d7a
Wesley <wesley.butura3@gmail.com>
2026-05-29 22:36:41 -0300

    feat: alteração da vulnerabilidade para teste de novo(XSS: Cross-Site Scripting)

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
-from flask import request
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
     name = request.args.get("name")
-    return f"<h1>Hello {name}</h1>"
+    return render_template_string(f"<h1>{name}</h1>")
